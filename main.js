7605819f18ddc212070343ebf735a9c96fb61321
JFossel <jfossel@conncoll.edu>
2023-02-02 20:38:57 -0500

    Commit of updated website code, set up through work experience. Changed layout of education and work expereince to utilize a grid view and dyamically adjsut the timeline line length using javascript.

--- main.js
+++ main.js
@@ -15,3 +15,35 @@ $('.enter_link').click(function () {
     
 });
 
+function getLineHeight() {
+    var r = document.querySelector(':root');
+    var rs = getComputedStyle(r);
+    console.log(rs.getPropertyValue('--line_height') )  
+  }
+
+  function setLineHeight(height) {
+    var r = document.querySelector(':root');
+    r.style.setProperty('--line_height', height + "px");
+  }
+
+  function getPosition(name){
+    var target = document.getElementById(name);
+    var rect = target.getBoundingClientRect();
+    console.log(rect.top, rect.right, rect.bottom, rect.left);
+    return [rect.top,rect.bottom];
+  }
+
+  function adjustHeights(){
+    getLineHeight();
+    setLineHeight(getPosition("icon_2")[0]-getPosition("icon_1")[1]); //Sets the height of line between two icons by taking the difference between both positions
+    getLineHeight();
+  }
+
+    var globalResizeTimer = null;
+
+  $(window).resize(function() {
+      if(globalResizeTimer != null) window.clearTimeout(globalResizeTimer);
+      globalResizeTimer = window.setTimeout(function() {
+        adjustHeights();
+      }, 100);
+  });
